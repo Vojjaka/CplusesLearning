f1742f9d8549fafd13eed25f1629c7fbd68ba385
Vladyslav Bezbakh <Vladyslav.Bezbakh@ubisoft.com>
2017-10-02 11:33:09 +0300

    Some code in main

---
+++ begginer/Tutorials/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	/*
+	string x;
+	cout << "Please enter an Int:";
+	cin >> x;
+	cout << "Thank you!" << endl;
+	cin >> x;
+	*/
+
+	
+
+	return 0;
+}
