c66e9aca1d6a0faa5759e93d16789e8fd27741a3
Vladyslav Bezbakh <Vladyslav.Bezbakh@ubisoft.com>
2017-10-02 17:09:53 +0300

    More code

--- begginer/Tutorials/main.cpp
+++ begginer/Tutorials/main.cpp
@@ -11,8 +11,51 @@ int main()
 	cout << "Thank you!" << endl;
 	cin >> x;
 	*/
-
+	/*
+	string y;
+	cout << "Sandwich Type: ";
+	getline(cin, y);
+	cout << "creating " << y;
+	cout << " Sandwich." << endl;
+	cout << "Press enter to exit. . .";
+	getline(cin, y);
+	*/
+	/*char x[100];
+	cin >> x;
+	cin.ignore(100, '\n');
+	cout << 111 << endl;
+	cin.getline(x, 100);
+	cout << 222 << endl;
+	cin >> x;
+	cout << 333 << endl;
+	cin >> x;*/
+	/*cout << 5 + 4 << endl;
+	cout << 5 - 4 << endl;
+	cout << 5 * 4 << endl;
+	cout << 5 / 4.0 << endl;
+	cout << 5 % 4 << endl;
+	int x = 5;
+	x += 8;
+	cout << x << endl;*/
+	/*int wounds = 0;
+	wounds++;
+	++wounds;
+	cout << wounds << endl;
+	wounds--;
+	cout << wounds << endl;*/
 	
+	/*cout << !true << endl;
+	cout << (5==4) << endl;
+	cout << (5 != 4) << endl;
+	cout << (5 < 4) << endl;
+	cout << (5 > 4) << endl;
+	cout << (5 <= 4) << endl;
+	cout << (5 >= 4) << endl;
+	cout << (true && true) << endl;
+	cout << (true || true) << endl;
+*/
+	string y;
+	getline(cin, y);
 
 	return 0;
 }
